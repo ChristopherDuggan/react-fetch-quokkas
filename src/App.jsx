7015ab2417e7097cdb7f0b1209695c46d686f74b
ChristopherDuggan <christopherduggan@gmail.com>
2024-10-15 11:25:22 -0400

    implement weatherSearch in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
 import * as weatherService from './services/weatherService';
 
+import WeatherSearch from './components/WeatherSearch';
+
 const App = () => {
-  const fetchData = async () => {
-    const data = await weatherService.show('New York');
-    console.log('Data:', data);
+  const [weather, setWeather] = useState({});
+
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    };
+    setWeather(newWeatherState);
   };
   return (
     <main>
       <h1>Weather API</h1>
-      <button onClick={fetchData}>Fetch Weather Data</button>
+      <WeatherSearch fetchData={fetchData} />
     </main>
   );
 }
