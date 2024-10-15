7db0c18e7006cb02cbaa34e9c3c2fb7a8f117b7e
ChristopherDuggan <christopherduggan@gmail.com>
2024-10-15 11:01:17 -0400

    create button for fetching weather in New York in App.jsx

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -6,11 +6,12 @@ const show = async (city) => {
     const queryString = `&q=${city}`;
     const res = await fetch(BASE_URL + queryString);
     const data = await res.json();
-    console.log('Data:', data);
     return data;
   } catch (err) {
     console.log(err);
   }
 };
+
+
 
 export { show };
